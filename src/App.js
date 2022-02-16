69b7b11b8f258470a6410662507cb294095af483
Marcelus <mebsport@gmail.com>
2022-02-16 09:10:04 -0500

    sum stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+const api = {
+  key: '094b3c64ca57bc590c163d710954a0f1',
+  base: 'api.openweathermap.org/data/2.5/',
+};
+
 function App() {
-  return <div className='App'>idk</div>;
+  return (
+    <div className='app'>
+      <main>
+        <div className='search-box'>
+          <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
