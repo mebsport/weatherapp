1a0efe97e48f8225a86f99c71fb6c3015c1b07b1
Marcelus <mebsport@gmail.com>
2022-02-17 09:18:03 -0500

    Function: Show Date & Location
    The date and location is shown on the page right underneath the search bar.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,49 @@ const api = {
 };
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${month} ${date}, ${year}`;
+  };
+
   return (
     <div className='app'>
       <main>
         <div className='search-box'>
           <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+        <div>
+          <div className='location-box'>
+            <div className='location'>Boston, US</div>
+            <div className='date'>{dateBuilder(new Date())}</div>
+          </div>
         </div>
       </main>
     </div>
